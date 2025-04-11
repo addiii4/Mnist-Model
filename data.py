4456839ef12bee14308345e71e3f9cc8f64fa91e
addiii4 <adityacheema4@gmail.com>
2025-04-12 01:30:34 +1000

    Loaded dataset and outputting sample with matplotlib

---
+++ data.py
@@ -0,0 +1,31 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+# Transform to tensor and normalize between [-1, 1]
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+# Load datasets
+train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+test_dataset  = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader  = DataLoader(test_dataset, batch_size=1000, shuffle=False)
+
+# Visualize a few examples
+examples = enumerate(train_loader)
+batch_idx, (example_data, example_targets) = next(examples)
+
+fig = plt.figure()
+for i in range(6):
+    plt.subplot(2,3,i+1)
+    plt.tight_layout()
+    plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+    plt.title(f"Label: {example_targets[i]}")
+    plt.xticks([])
+    plt.yticks([])
+plt.show()
